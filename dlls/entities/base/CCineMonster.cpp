f56bc8e5395e6aa48e1bb90a0e153020bd11e7cc
Dominik Madarász <zaklaus@outlook.com>
2021-03-16 09:04:45 +0100

    Update CCineMonster.cpp

--- dlls/entities/base/CCineMonster.cpp
+++ dlls/entities/base/CCineMonster.cpp
@@ -576,8 +576,11 @@ void ScriptEntityCancel(edict_t* pentCine)
     // make sure they are a scripted_sequence
     if (FClassnameIs(pentCine, "scripted_sequence") || FClassnameIs(pentCine, "scripted_action"))
     {
-        ((CCineMonster*)VARS(pentCine))->m_iState = STATE_OFF;
         CCineMonster* pCineTarget = GetClassPtr((CCineMonster*)VARS(pentCine));
+        if (!pCineTarget->pev)
+            return;
+        pCineTarget->m_iState = STATE_OFF;
+
         // make sure they have a monster in mind for the script
         CBaseEntity* pEntity = pCineTarget->m_hTargetEnt;
         CBaseMonster* pTarget = NULL;
