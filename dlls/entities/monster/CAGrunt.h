c5e4e69165c06e36088ef14bcc7291163c814337
Daniel Walder <daniel.walder@gmail.com>
2024-07-21 13:52:12 +1000

    agrunt.cpp

--- dlls/entities/monster/CAGrunt.h
+++ dlls/entities/monster/CAGrunt.h
@@ -13,3 +13,110 @@
 *
 ****/
 
+#pragma once
+
+#include "entities\CSquadMonster.h"
+
+/**
+ * monster-specific schedule types
+ */
+enum
+{
+	SCHED_AGRUNT_SUPPRESS = LAST_COMMON_SCHEDULE + 1,
+	SCHED_AGRUNT_THREAT_DISPLAY,
+};
+
+/**
+ * monster-specific tasks
+ */
+enum
+{
+	TASK_AGRUNT_SETUP_HIDE_ATTACK = LAST_COMMON_TASK + 1,
+	TASK_AGRUNT_GET_PATH_TO_ENEMY_CORPSE,
+};
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+
+#define AGRUNT_AE_HORNET1 (1)
+#define AGRUNT_AE_HORNET2 (2)
+#define AGRUNT_AE_HORNET3 (3)
+#define AGRUNT_AE_HORNET4 (4)
+#define AGRUNT_AE_HORNET5 (5)
+// some events are set up in the QC file that aren't recognized by the code yet.
+#define AGRUNT_AE_PUNCH (6)
+#define AGRUNT_AE_BITE (7)
+
+#define AGRUNT_AE_LEFT_FOOT (10)
+#define AGRUNT_AE_RIGHT_FOOT (11)
+
+#define AGRUNT_AE_LEFT_PUNCH (12)
+#define AGRUNT_AE_RIGHT_PUNCH (13)
+
+#define AGRUNT_MELEE_DIST 100
+
+// LRC - body definitions for the Agrunt model
+#define AGRUNT_BODY_HASGUN 0
+#define AGRUNT_BODY_NOGUN 1
+
+/**
+ * Agrunt - Dominant, warlike alien grunt monster
+ */
+class CAGrunt : public CSquadMonster
+{
+public:
+	void Spawn() override;
+	void Precache() override;
+	void SetYawSpeed() override;
+	int Classify() override;
+	int ISoundMask() override;
+	void HandleAnimEvent(MonsterEvent_t* pEvent) override;
+	void SetObjectCollisionBox() override
+	{
+		pev->absmin = pev->origin + Vector(-32, -32, 0);
+		pev->absmax = pev->origin + Vector(32, 32, 85);
+	}
+
+	Schedule_t* GetSchedule() override;
+	Schedule_t* GetScheduleOfType(int Type) override;
+	bool FCanCheckAttacks() override;
+	bool CheckMeleeAttack1(float flDot, float flDist) override;
+	bool CheckRangeAttack1(float flDot, float flDist) override;
+	void StartTask(Task_t* pTask) override;
+	void AlertSound() override;
+	void DeathSound() override;
+	void PainSound() override;
+	void AttackSound();
+	void PrescheduleThink() override;
+	void TraceAttack(entvars_t* pevAttacker, float flDamage, Vector vecDir, TraceResult* ptr, int bitsDamageType) override;
+	int IRelationship(CBaseEntity* pTarget) override;
+	void StopTalking();
+	bool ShouldSpeak();
+	void Killed(entvars_t* pevAttacker, int iGib) override;
+
+
+	CUSTOM_SCHEDULES;
+
+	bool Save(CSave& save) override;
+	bool Restore(CRestore& restore) override;
+	static TYPEDESCRIPTION m_SaveData[];
+
+	static const char* pAttackHitSounds[];
+	static const char* pAttackMissSounds[];
+	static const char* pAttackSounds[];
+	static const char* pDieSounds[];
+	static const char* pPainSounds[];
+	static const char* pIdleSounds[];
+	static const char* pAlertSounds[];
+
+	bool m_fCanHornetAttack;
+	float m_flNextHornetAttackCheck;
+
+	float m_flNextPainTime;
+
+	// three hacky fields for speech stuff. These don't really need to be saved.
+	float m_flNextSpeakTime;
+	float m_flNextWordTime;
+	int m_iLastWord;
+};
