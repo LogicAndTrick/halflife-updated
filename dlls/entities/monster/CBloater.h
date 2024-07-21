160a8fe21e341a247d1996046ded94204db7c63f
Daniel Walder <daniel.walder@gmail.com>
2024-07-21 16:16:56 +1000

    bloater.cpp

--- dlls/entities/monster/CBloater.h
+++ dlls/entities/monster/CBloater.h
@@ -1,15 +1,43 @@
 /***
-*
-*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
-*
-*   Use, distribution, and modification of this source code and/or resulting
-*   object code is restricted to non-commercial enhancements to products from
-*   Valve LLC.  All other use, distribution, or modification is prohibited
-*   without written permission from Valve LLC.
-*
-****/
+ *
+ *	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+ *
+ *	This product contains software technology licensed from Id
+ *	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+ *	All Rights Reserved.
+ *
+ *   Use, distribution, and modification of this source code and/or resulting
+ *   object code is restricted to non-commercial enhancements to products from
+ *   Valve LLC.  All other use, distribution, or modification is prohibited
+ *   without written permission from Valve LLC.
+ *
+ ****/
 
+#pragma once
+
+#include "entities/CBaseMonster.h"
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+#define BLOATER_AE_ATTACK_MELEE1 0x01
+
+class CBloater : public CBaseMonster
+{
+public:
+	void Spawn() override;
+	void Precache() override;
+	void SetYawSpeed() override;
+	int Classify() override;
+	void HandleAnimEvent(MonsterEvent_t* pEvent) override;
+
+	void PainSound() override;
+	void AlertSound() override;
+	void IdleSound() override;
+	void AttackSnd();
+
+	// No range attacks
+	bool CheckRangeAttack1(float flDot, float flDist) override { return false; }
+	bool CheckRangeAttack2(float flDot, float flDist) override { return false; }
+	bool TakeDamage(entvars_t* pevInflictor, entvars_t* pevAttacker, float flDamage, int bitsDamageType) override;
+};
