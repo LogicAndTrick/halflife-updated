d9e43f521e97c707d8e116a31638d6b5c219b040
Daniel Walder <daniel.walder@gmail.com>
2024-07-22 21:27:23 +1000

    zombie.cpp

--- dlls/entities/monster/CZombie.h
+++ dlls/entities/monster/CZombie.h
@@ -1,15 +1,57 @@
 /***
-*
-*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
-*
-*   Use, distribution, and modification of this source code and/or resulting
-*   object code is restricted to non-commercial enhancements to products from
-*   Valve LLC.  All other use, distribution, or modification is prohibited
-*   without written permission from Valve LLC.
-*
-****/
+ *
+ *	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+ *
+ *	This product contains software technology licensed from Id
+ *	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+ *	All Rights Reserved.
+ *
+ *   Use, distribution, and modification of this source code and/or resulting
+ *   object code is restricted to non-commercial enhancements to products from
+ *   Valve LLC.  All other use, distribution, or modification is prohibited
+ *   without written permission from Valve LLC.
+ *
+ ****/
 
+#pragma once
+
+#include "entities/CBaseMonster.h"
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+#define ZOMBIE_AE_ATTACK_RIGHT 0x01
+#define ZOMBIE_AE_ATTACK_LEFT 0x02
+#define ZOMBIE_AE_ATTACK_BOTH 0x03
+
+#define ZOMBIE_FLINCH_DELAY 2 // at most one flinch every n secs
+
+class CZombie : public CBaseMonster
+{
+public:
+	void Spawn() override;
+	void Precache() override;
+	void SetYawSpeed() override;
+	int Classify() override;
+	void HandleAnimEvent(MonsterEvent_t* pEvent) override;
+	int IgnoreConditions() override;
+
+	float m_flNextFlinch;
+
+	void PainSound() override;
+	void AlertSound() override;
+	void IdleSound() override;
+	void AttackSound();
+
+	static const char* pAttackSounds[];
+	static const char* pIdleSounds[];
+	static const char* pAlertSounds[];
+	static const char* pPainSounds[];
+	static const char* pAttackHitSounds[];
+	static const char* pAttackMissSounds[];
+
+	// No range attacks
+	bool CheckRangeAttack1(float flDot, float flDist) override { return false; }
+	bool CheckRangeAttack2(float flDot, float flDist) override { return false; }
+	bool TakeDamage(entvars_t* pevInflictor, entvars_t* pevAttacker, float flDamage, int bitsDamageType) override;
+};
