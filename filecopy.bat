1e41cd85de3e02397a8d99fb463a1ceb67d3284b
Daniel Walder <daniel.walder@gmail.com>
2024-08-11 19:00:53 +1000

    Use registry variables in the filecopy script

--- filecopy.bat
+++ filecopy.bat
@@ -1,8 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+
+set KEY_NAME="HKEY_CURRENT_USER\SOFTWARE\Valve\Steam"
+set VALUE_NAME=ModInstallPath
+
+FOR /F "tokens=2*" %%A IN ('REG.exe query "%KEY_NAME%" /v "%VALUE_NAME%"') DO (set hl_install_dir=%%B)
+echo %hl_install_dir%
+
 rem Set this path to your mod directory. You can use environment variables to avoid hardcoding the path if your team members have different install locations.
-set mod_directory=C:/Program Files (x86)/Steam/steamapps/common/Half-Life/spirit-level
+set mod_directory=%hl_install_dir%/spirit-level
+
+echo %mod_directory%
 
 rem Input paths may end with a backslash which will be interpreted as an escape character when passed into robocopy, so this needs to be escaped.
 set source=%~1
